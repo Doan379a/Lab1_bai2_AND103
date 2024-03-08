b896aceacad5107d0ec437d824df9078f21ad869
Doan379a <doan379a@gmail.com>
2024-03-08 10:03:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/lab2/model.java
@@ -0,0 +1,20 @@
+package com.example.lab2;
+
+public class model {
+    private String name;
+
+    public model(String name) {
+        this.name = name;
+    }
+
+    public model() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
